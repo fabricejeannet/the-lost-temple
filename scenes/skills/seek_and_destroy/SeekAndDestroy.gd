1b7d6098fd536c843cb9f607b97fcbe3d7441386
fabricejeannet <fabricejeannet@gmail.com>
2022-07-21 16:45:33 +0200

    Starting to implement attack behavior for Seek And Destroy skill

--- scenes/skills/seek_and_destroy/SeekAndDestroy.gd
+++ scenes/skills/seek_and_destroy/SeekAndDestroy.gd
@@ -7,6 +7,8 @@ var path:Array
 var motion
 var orientation 
 var closest_enemy
+var _in_attack_range:bool = false
+var target = null
 
 export var speed:float = 200.0
 export var damage:float = 100.0
@@ -15,7 +17,7 @@ export var min_distance:float = 10.0
 
 onready var Nodes = get_node("/root/Nodes")
 onready var path_line = $PathLine
-onready var timer = $Timer
+onready var timer = $LifeTimer
 onready var walk_animation_manager = $WalkAnimationManager
 onready var animation_player = $AnimationPlayer
 
@@ -51,15 +53,36 @@ func _physics_process(delta):
 				
 			position += motion
 
-
-	orientation = walk_animation_manager.get_orientation_according_to(motion)
-	walk_animation_manager.play_animation_corresponding_to_orientation(animation_player, orientation)
+	if has_a_target() and is_in_attack_range():
+		animation_player.play("idle") # A remplacer par l'animation d'attaque	
+	else:
+		orientation = walk_animation_manager.get_orientation_according_to(motion)
+		walk_animation_manager.play_animation_corresponding_to_orientation(animation_player, orientation)
+	
 
 
 func _on_SeekAndDestroy_body_entered(body):
-	if body.is_in_group("enemy"):
-		body.hurt(damage)
+	if body.is_in_group("enemy") and has_a_target():
+		_in_attack_range = true
+		target = body
+		target.hurt(damage)
+
+
+func _on_SeekAndDestroy_body_exited(body):
+	if body == target:
+		target = null
+		_in_attack_range = false
+
+
+func has_a_target() -> bool:
+	return is_instance_valid(target)
+
+
+func is_in_attack_range() -> bool:
+	return _in_attack_range
 
 
 func _on_Timer_timeout():
 	call_deferred("queue_free")
+
+
