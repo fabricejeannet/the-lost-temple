25ef32a9c6920bf21f0f270ea9ccaad7ac90f9e7
fabricejeannet <fabricejeannet@gmail.com>
2022-09-06 12:19:12 +0200

    Still implementing infinite map

--- scenes/world/World.gd
+++ scenes/world/World.gd
@@ -1,1 +1,16 @@
 extends Node2D
+
+var old_virtual_position:Vector2
+var virtual_position:Vector2
+
+onready var level = $Level
+
+func _process(_delta):
+	virtual_position = level.world_to_map(Nodes.player.position)
+	if virtual_position != old_virtual_position:
+		Logger.debug("Map position : " + str(virtual_position))
+		old_virtual_position = virtual_position
+
+
+#func set_level(_level:TileMap) -> void:
+#	var 
