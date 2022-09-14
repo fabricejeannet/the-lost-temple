48143bf700c4d591ff6faa0963164fbadb08e7af
fabricejeannet <fabricejeannet@gmail.com>
2022-09-14 11:54:51 +0200

    still implementing infinite map

--- scenes/world/InfiniteTileMap.gd
+++ scenes/world/InfiniteTileMap.gd
@@ -1,9 +1,33 @@
 extends TileMap
 
 var template:TileMap
+var width_in_tiles
+var height_in_tiles
+
+
+func _ready():
+#	 template = Levels.instance().get_node("0")
+	pass
 
 func set_template(_template:TileMap) -> void:
 	template = _template
+	var template_used_rect = template.get_used_rect()
+	width_in_tiles = template_used_rect.size.x
+	height_in_tiles = template_used_rect.size.y
 
 
+func get_virtual_position_from(given_position:Vector2) -> Vector2:
+	var given_x = given_position.x
+	if given_x < 0:
+		given_x += width_in_tiles
+	var given_y = given_position.y
+	if given_y < 0:
+		given_y += height_in_tiles
+	return Vector2(fmod(given_x, width_in_tiles), fmod(given_y, height_in_tiles))
 
+
+func set_tiles_for_player_at(given_position:Vector2) -> void:
+	clear()
+	for x in range(given_position.x - width_in_tiles, given_position.x + width_in_tiles):
+		for y in range(given_position.y - height_in_tiles, given_position.y + height_in_tiles):
+			set_cell(x, y, template.get_cellv(get_virtual_position_from(Vector2(x,y))))
--- test/unit/test_infinite_tilemap.gd
+++ test/unit/test_infinite_tilemap.gd
@@ -8,15 +8,51 @@ var infinite_tilemap:TileMap
 
 
 func before_each():
-	template = Levels.instance().get_node("0")
+#	template = Levels.instance().get_node("0")
+	var tile_type = 0
+	template = TileMap.new()
+	for x in range (0, 32):
+		for y in range (0, 32):
+			template.set_cell(x, y, tile_type)
+			tile_type += 1		
+	
+	template.update_dirty_quadrants()
 	infinite_tilemap = InfiniteTileMap.new()	
+	infinite_tilemap.set_template(template)
+
+
+func test_template_has_1024_different_type_of_tile() -> void : 
+	assert_eq(template.get_cell(0, 0), 0)
+	assert_eq(template.get_cell(31, 31), 1023)
 
 
 func test_can_set_a_template() -> void:
-	infinite_tilemap.set_template(template)
 	assert_eq(infinite_tilemap.template, template)
-#	assert_eq(infinite_tilemap.width, template.get_used_rect().size.x)
+	assert_eq(infinite_tilemap.width_in_tiles, 32)
+	assert_eq(infinite_tilemap.height_in_tiles, 32)
 
+
+func test_virtual_positions() -> void :
+	var position_on_map
+	var virtual_position
+
+	position_on_map = Vector2(32,16)
+	virtual_position = infinite_tilemap.get_virtual_position_from(position_on_map)
+	assert_eq(virtual_position, Vector2(0, 16))
+
+	position_on_map = Vector2(-1,16)
+	virtual_position = infinite_tilemap.get_virtual_position_from(position_on_map)
+	assert_eq(virtual_position, Vector2(31, 16))
+
+	position_on_map = Vector2(-3,-5)
+	virtual_position = infinite_tilemap.get_virtual_position_from(position_on_map)
+	assert_eq(virtual_position, Vector2(29, 27))
+
+
+func test_set_tiles() -> void:
+	infinite_tilemap.set_tiles_for_player_at(Vector2(16,16))
+	var used_rect = infinite_tilemap.get_used_rect()
+	assert_eq(used_rect.size, Vector2(64,64))
 
 func after_each():
 	template.free()
